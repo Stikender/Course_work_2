12291af589ef67935a17df2efd17b427659a0391
Stikender <nadik17@bk.ru>
2023-02-20 14:43:48 +0800

    Finish commit

--- src/TaskFunctionalProgram/Main.java
+++ src/TaskFunctionalProgram/Main.java
@@ -11,6 +11,7 @@ import static java.util.Map.Entry.comparingByKey;
 
 public class Main {
     public static void main(String[] args) {
+
         String s = "yourapp the quick brown fox jumps over the lazy dog";
         System.out.println("TOP 10:");
         Arrays.stream(s.split(" "))
